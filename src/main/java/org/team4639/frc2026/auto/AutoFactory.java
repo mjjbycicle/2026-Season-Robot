2a3f0f81bf6efb5da8e44c67a25a5a15b7cede5f
Aditya <adityakhambekar9@gmail.com>
2026-01-20 03:17:59 -0600

    Comments

--- src/main/java/org/team4639/frc2026/auto/AutoFactory.java
+++ src/main/java/org/team4639/frc2026/auto/AutoFactory.java
@@ -7,6 +7,10 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public class AutoFactory {
+    // Looks good, just change the names. DS_TL can be a little confusing, so be as descriptive as possible
+    // at least until we come up with a naming scheme for the paths. Same thing with the trajectory names
+    // as much as possible try and have them be the same as the command name and both be easily 
+    // understandable.
     public static Command DS_TL(){
         try{
             PathPlannerPath path = PathPlannerPath.fromChoreoTrajectory("NewPath");
