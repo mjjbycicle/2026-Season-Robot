f8ddca1901c782448fb1463ab6fd323a460417a5
Aditya <adityakhambekar9@gmail.com>
2026-01-30 18:13:28 -0600

    drivetrain

--- src/main/java/org/team4639/frc2026/BuildConstants.java
+++ src/main/java/org/team4639/frc2026/BuildConstants.java
@@ -6,6 +6,7 @@ package org.team4639.frc2026;
  * Automatically generated file containing build version information.
  */
 public final class BuildConstants {
+<<<<<<< HEAD
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-Season-Robot";
   public static final String VERSION = "unspecified";
@@ -16,6 +17,18 @@ public final class BuildConstants {
   public static final String BUILD_DATE = "2026-02-07 22:08:41 CST";
   public static final long BUILD_UNIX_TIME = 1770523721788L;
   public static final int DIRTY = 1;
+=======
+    public static final String MAVEN_GROUP = "";
+    public static final String MAVEN_NAME = "2026-Season-Robot";
+    public static final String VERSION = "unspecified";
+    public static final int GIT_REVISION = 16;
+    public static final String GIT_SHA = "f3b9c0fb4da27f88d41517a684254a1b5d848d09";
+    public static final String GIT_DATE = "2026-01-25 20:21:48 CST";
+    public static final String GIT_BRANCH = "dev-drivetrain";
+    public static final String BUILD_DATE = "2026-01-30 17:38:06 CST";
+    public static final long BUILD_UNIX_TIME = 1769816286761L;
+    public static final int DIRTY = 1;
+>>>>>>> a859009 (drivetrain)
 
   private BuildConstants(){}
 }
