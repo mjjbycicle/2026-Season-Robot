0a47e0080960977e43291e5a94bd7b0498825ae6
4639prog <outreach@team4639.org>
2026-01-31 19:50:10 -0600

    fix drive ratio

--- src/main/java/org/team4639/frc2026/BuildConstants.java
+++ src/main/java/org/team4639/frc2026/BuildConstants.java
@@ -9,12 +9,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "2026-Season-Robot";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 16;
-    public static final String GIT_SHA = "f3b9c0fb4da27f88d41517a684254a1b5d848d09";
-    public static final String GIT_DATE = "2026-01-25 20:21:48 CST";
+    public static final int GIT_REVISION = 17;
+    public static final String GIT_SHA = "c744f066918da97a8c2d70137aef01f616d15a6d";
+    public static final String GIT_DATE = "2026-01-30 18:13:28 CST";
     public static final String GIT_BRANCH = "dev-drivetrain";
-    public static final String BUILD_DATE = "2026-01-30 17:38:06 CST";
-    public static final long BUILD_UNIX_TIME = 1769816286761L;
+    public static final String BUILD_DATE = "2026-01-31 19:07:35 CST";
+    public static final long BUILD_UNIX_TIME = 1769908055549L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
