983b0fecf2c0b748ba8ed82d147fbd2647d0eae1
Michael <nengzhimj@gmail.com>
2026-02-06 16:46:57 -0600

    finished Shooter subsystem

--- src/main/java/org/team4639/frc2026/BuildConstants.java
+++ src/main/java/org/team4639/frc2026/BuildConstants.java
@@ -1,21 +1,19 @@
-/* Copyright (c) 2025-2026 FRC 4639. */
-
 package org.team4639.frc2026;
 
 /**
  * Automatically generated file containing build version information.
  */
 public final class BuildConstants {
-    public static final String MAVEN_GROUP = "";
-    public static final String MAVEN_NAME = "2026-Season-Robot";
-    public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 3;
-    public static final String GIT_SHA = "99b2a84268a8bfae8f3471754c7a5ad86fecf024";
-    public static final String GIT_DATE = "2026-02-06 09:29:08 CST";
-    public static final String GIT_BRANCH = "main";
-    public static final String BUILD_DATE = "2026-02-06 16:19:01 CST";
-    public static final long BUILD_UNIX_TIME = 1770416341466L;
-    public static final int DIRTY = 1;
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2026-Season-Robot";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 20;
+  public static final String GIT_SHA = "4e14c68fc0d36fb4aa0e1bbf315e30775400b239";
+  public static final String GIT_DATE = "2026-02-06 16:25:09 CST";
+  public static final String GIT_BRANCH = "dev";
+  public static final String BUILD_DATE = "2026-02-06 16:26:58 CST";
+  public static final long BUILD_UNIX_TIME = 1770416818690L;
+  public static final int DIRTY = 1;
 
-    private BuildConstants() {}
+  private BuildConstants(){}
 }
--- src/main/java/org/team4639/frc2026/subsystems/shooter/Shooter.java
+++ src/main/java/org/team4639/frc2026/subsystems/shooter/Shooter.java
@@ -2,9 +2,19 @@
 
 package org.team4639.frc2026.subsystems.shooter;
 
-public class Shooter {
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
+import org.team4639.frc2026.RobotState;
+
+public class Shooter extends SubsystemBase {
+    private final RobotState state;
     private final ShooterIO io;
     private final ShooterIOInputsAutoLogged inputs = new ShooterIOInputsAutoLogged();
+
+    private final double PASSING_RPM = 0;
+    private final double IDLE_VOLTAGE = 0;
+    private double SCORING_RPM = 0;
 
     public enum WantedState {
         OFF,
@@ -23,5 +33,72 @@ public class Shooter {
     private WantedState wantedState = WantedState.OFF;
     private SystemState systemState = SystemState.OFF;
 
-    public Shooter(ShooterIO io, )
+    public Shooter(ShooterIO io, RobotState state) {
+        this.io = io;
+        this.state = state;
+    }
+
+    @Override
+    public void periodic() {
+        io.updateInputs(inputs);
+        Logger.processInputs("Shooter", inputs);
+        SystemState newState = handleStateTransitions();
+        if (newState != systemState) {
+            Logger.recordOutput("Shooter/SystemState", newState.toString());
+            systemState = newState;
+        }
+
+        if (DriverStation.isDisabled()) {
+            systemState = SystemState.OFF;
+        }
+
+        switch (systemState) {
+            case OFF:
+                handleOff();
+                break;
+            case IDLE:
+                handleIdle();
+                break;
+            case SCORING:
+                handleScoring();
+                break;
+            case PASSING:
+                handlePassing();
+                break;
+        }
+    }
+
+    private SystemState handleStateTransitions() {
+        return switch (wantedState) {
+            case SCORING -> SystemState.SCORING;
+            case PASSING -> SystemState.PASSING;
+            case IDLE -> SystemState.IDLE;
+            default -> SystemState.OFF;
+        };
+    }
+
+    private void handleOff() {
+        io.setVoltage(0);
+    }
+
+    private void handleScoring() {
+        io.setRPM(this.SCORING_RPM);
+    }
+
+    private void handlePassing() {
+        io.setRPM(PASSING_RPM);
+    }
+
+    private void handleIdle() {
+        io.setVoltage(IDLE_VOLTAGE);
+    }
+
+    private void setWantedState(WantedState wantedState) {
+        this.wantedState = wantedState;
+    }
+
+    private void setWantedState(WantedState wantedState, double scoringRPM) {
+        setWantedState(wantedState);
+        this.SCORING_RPM = scoringRPM;
+    }
 }
