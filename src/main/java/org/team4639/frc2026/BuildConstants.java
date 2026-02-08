c09ef03aedf45c0830bf6e29e6dc5a0e51ecbd5c
Michael Jiang <nengzhimj@gmail.com>
2026-02-07 22:09:25 -0600

    fixed build (palantir formatter sucks)

--- src/main/java/org/team4639/frc2026/BuildConstants.java
+++ src/main/java/org/team4639/frc2026/BuildConstants.java
@@ -6,16 +6,16 @@ package org.team4639.frc2026;
  * Automatically generated file containing build version information.
  */
 public final class BuildConstants {
-    public static final String MAVEN_GROUP = "";
-    public static final String MAVEN_NAME = "2026-Season-Robot";
-    public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 23;
-    public static final String GIT_SHA = "c700bd7466432de2c314c33719d16d0e3ea76eae";
-    public static final String GIT_DATE = "2026-02-06 20:56:54 CST";
-    public static final String GIT_BRANCH = "dev-shooter";
-    public static final String BUILD_DATE = "2026-02-07 12:49:55 CST";
-    public static final long BUILD_UNIX_TIME = 1770490195085L;
-    public static final int DIRTY = 1;
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2026-Season-Robot";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 24;
+  public static final String GIT_SHA = "6fe98503dc0180baf3802d2005674d08b0cf0339";
+  public static final String GIT_DATE = "2026-02-07 22:04:07 CST";
+  public static final String GIT_BRANCH = "dev-shooter";
+  public static final String BUILD_DATE = "2026-02-07 22:08:41 CST";
+  public static final long BUILD_UNIX_TIME = 1770523721788L;
+  public static final int DIRTY = 1;
 
-    private BuildConstants() {}
+  private BuildConstants(){}
 }
