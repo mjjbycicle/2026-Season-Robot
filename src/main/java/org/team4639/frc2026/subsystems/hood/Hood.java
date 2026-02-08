b2b6babd855b60ffa9cd76ab841c61192daeaba6
Michael Jiang <nengzhimj@gmail.com>
2026-02-08 10:16:28 -0600

    added necessary utils

--- src/main/java/org/team4639/frc2026/subsystems/hood/Hood.java
+++
@@ -1,5 +0,0 @@
-/* Copyright (c) 2025-2026 FRC 4639. */
-
-package org.team4639.frc2026.subsystems.hood;
-
-public class Hood {}
--- src/main/java/org/team4639/frc2026/subsystems/kicker/KickerIOTalonFX.java
+++ src/main/java/org/team4639/frc2026/subsystems/kicker/KickerIOTalonFX.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import org.team4639.frc2026.util.PortConfiguration;
 
 public class KickerIOTalonFX implements KickerIO {
     private final TalonFX kickerMotor;
